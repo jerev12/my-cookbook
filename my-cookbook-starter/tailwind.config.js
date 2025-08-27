d66992b6029dc4e44cfbf8b99a20002a58a0d55a
jerev12 <jerevoy@gmail.com>
2025-08-26 21:10:46 -0400

    Create tailwind.config.js

---
+++ my-cookbook-starter/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/components/**/*.{js,ts,jsx,tsx}", // add this if your Modal is here
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
