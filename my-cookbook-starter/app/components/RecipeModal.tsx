cc792d8882146e11c8a4e57551e64009459fe04a
jerev12 <jerevoy@gmail.com>
2025-08-28 17:14:16 -0400

    Update RecipeModal.tsx

--- my-cookbook-starter/app/components/RecipeModal.tsx
+++ my-cookbook-starter/app/components/RecipeModal.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import Modal from '@/components/Modal';
+import Modal from '../components/Modal';
 import { supabase } from '@/lib/supabaseClient';
 
 type Recipe = {
