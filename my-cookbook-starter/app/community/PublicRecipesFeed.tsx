fe854d0c915c31cb18470f64eb669af5a08f58cd
jerev12 <jerevoy@gmail.com>
2025-08-28 22:27:09 -0400

    Update PublicRecipesFeed.tsx

--- my-cookbook-starter/app/community/PublicRecipesFeed.tsx
+++ my-cookbook-starter/app/community/PublicRecipesFeed.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { supabase } from '@/lib/supabaseClient';
-import RecipeModal from '../app/components/RecipeModal';
+import RecipeModal from '../components/RecipeModal';
 import RecipeBadges from '../components/RecipeBadges';
 
 type Recipe = {
